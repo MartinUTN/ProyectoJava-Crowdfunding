4793fd93c6052c16a1f2e527f01a44d5fa0509fe
agussantinelli <agustinsantinelli@gmail.com>
2025-08-07 22:16:29 -0300

    Corrección Conexión Base de Datos

--- src/main/java/db/Conexion.java
+++ src/main/java/db/Conexion.java
@@ -3,11 +3,24 @@ package db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Conexion {
-    private static final String URL = "jdbc:mysql://localhost:3306/crowdfunding";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final Properties PROPS = new Properties();
+
+    static {
+        try (InputStream input = Conexion.class.getClassLoader().getResourceAsStream("database.properties")) {
+            PROPS.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static final String URL = PROPS.getProperty("db.url");
+    private static final String USER = PROPS.getProperty("db.user");
+    private static final String PASSWORD = PROPS.getProperty("db.password");
 
     public static Connection obtenerConexion() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
