fe5b343919980f2c2d260fcb96b0b9d051d5ba23
agussantinelli <agustinsantinelli@gmail.com>
2025-09-02 22:30:15 -0300

    Fix getters y setters Proyecto

--- src/java/modelo/Proyecto.java
+++ src/java/modelo/Proyecto.java
@@ -26,39 +26,100 @@ public class Proyecto {
     public void setCancelacion(Cancelacion_Proyecto cancelacion) {
         this.cancelacion = cancelacion;
     }
-    public int getIdProyecto() { return idProyecto; }
-    public void setIdProyecto(int idProyecto) { this.idProyecto = idProyecto; }
+    
+    public int getIdProyecto() {
+        return idProyecto;
+    }
+    
+    public void setIdProyecto(int idProyecto) {
+        this.idProyecto = idProyecto;
+    }
 
-    public String getNombreProyecto() { return nombreProyecto; }
-    public void setNombreProyecto(String nombreProyecto) { this.nombreProyecto = nombreProyecto; }
+    public String getNombreProyecto() {
+        return nombreProyecto;
+    }
+    
+    public void setNombreProyecto(String nombreProyecto) {
+        this.nombreProyecto = nombreProyecto;
+    }
 
-    public String getDescripcion() { return descripcion; }
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 
-    public BigDecimal getMontoMeta() { return montoMeta; }
-    public void setMontoMeta(BigDecimal montoMeta) { this.montoMeta = montoMeta; }
+    public BigDecimal getMontoMeta() {
+        return montoMeta;
+    }
+    
+    public void setMontoMeta(BigDecimal montoMeta) {
+        this.montoMeta = montoMeta;
+    }
 
-    public BigDecimal getMontoRecaudado() { return montoRecaudado; }
-    public void setMontoRecaudado(BigDecimal montoRecaudado) { this.montoRecaudado = montoRecaudado; }
+    public BigDecimal getMontoRecaudado() {
+        return montoRecaudado;
+    }
+    
+    public void setMontoRecaudado(BigDecimal montoRecaudado) {
+        this.montoRecaudado = montoRecaudado;
+    }
 
-    public LocalDate getFechaIni() { return fechaIni; }
-    public void setFechaIni(LocalDate fechaIni) { this.fechaIni = fechaIni; }
+    public LocalDate getFechaIni() {
+        return fechaIni;
+    }
+    
+    public void setFechaIni(LocalDate fechaIni) {
+        this.fechaIni = fechaIni;
+    }
 
-    public LocalDate getFechaFin() { return fechaFin; }
-    public void setFechaFin(LocalDate fechaFin) { this.fechaFin = fechaFin; }
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+    
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
 
-    public int getIdCreador() { return idCreador; }
-    public void setIdCreador(int idCreador) { this.idCreador = idCreador; }
+    public int getIdCreador() {
+        return idCreador;
+    }
+    
+    public void setIdCreador(int idCreador) {
+        this.idCreador = idCreador;
+    }
 
-    public String getEstado() { return estado; }
-    public void setEstado(String estado) { this.estado = estado; }
+    public String getEstado() {
+        return estado;
+    }
+    
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 
-    public String getFoto() { return foto; }
-    public void setFoto(String foto) { this.foto = foto; }
+    public String getFoto() {
+        return foto;
+    }
+    
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
 
-    public Categoria getCategoria() { return categoria; }
-    public void setCategoria(Categoria categoria) { this.categoria = categoria; }
+    public Categoria getCategoria() {
+        return categoria;
+    }
+    
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 
-    public Pais getPais() { return pais; }
-    public void setPais(Pais pais) { this.pais = pais; }
+    public Pais getPais() {
+        return pais;
+    }
+    
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
 }
